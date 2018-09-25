f29697d70d6f9d4715ec0d124532f71796ff12f5
riccardo venturini <riccardoventurini@riccardos-mbp.home>
2018-09-25 07:17:44 +0100

    fixing splash screen

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,13 +15,13 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel='apple-touch-startup-image' href='/apple_splash_2048.png' media='(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)' },
-      { rel='apple-touch-startup-image' href='/apple_splash_1668.png' media='(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)' },
-      { rel='apple-touch-startup-image' href='/apple_splash_1536.png' media='(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)' },
-      { rel='apple-touch-startup-image' href='/apple_splash_1125.png' media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)' },
-      { rel='apple-touch-startup-image' href='/apple_splash_1242.png' media='(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)' },
-      { rel='apple-touch-startup-image' href='/apple_splash_750.png' media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)' },
-      { rel='apple-touch-startup-image' href='/apple_splash_640.png' media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)' }
+      { rel='apple-touch-startup-image' href='/apple_splash_2048.png' },
+      { rel='apple-touch-startup-image' href='/apple_splash_1668.png' },
+      { rel='apple-touch-startup-image' href='/apple_splash_1536.png' },
+      { rel='apple-touch-startup-image' href='/apple_splash_1125.png' },
+      { rel='apple-touch-startup-image' href='/apple_splash_1242.png' },
+      { rel='apple-touch-startup-image' href='/apple_splash_750.png' },
+      { rel='apple-touch-startup-image' href='/apple_splash_640.png' }
     ]
   },
 
