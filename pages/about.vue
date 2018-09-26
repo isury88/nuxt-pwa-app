5c71bb6be2ce728118942195c5c3eb78780336a1
riccardo venturini <riccardoventurini@riccardos-mbp.home>
2018-09-26 06:50:49 +0100

    slideshow added

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
     <h1>About</h1>
+    <carousel>
+      <slide>
+        <img src="https://source.unsplash.com/random" alt="">
+      </slide>
+      <slide>
+        <img src="https://source.unsplash.com/random" alt="">
+      </slide>
+      <slide>
+        <img src="https://source.unsplash.com/random" alt="">
+      </slide>
+      <slide>
+        <img src="https://source.unsplash.com/random" alt="">
+      </slide>
+      <slide>
+        <img src="https://source.unsplash.com/random" alt="">
+      </slide>
+    </carousel>
   </div>
 </template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+export default {
+components: {
+  Carousel,
+  Slide
+}
+};
+</script>
